decc48bb3077ce3c2dcb8b821a76c0fee358076f
Rohit Mehra <RohitMehra26@outlook.com>
2025-07-26 02:01:58 +0530

    GET commit

--- lambda/new-smartcomm-check-status/index.py
+++ lambda/new-smartcomm-check-status/index.py
@@ -1,7 +1,17 @@
 import json
 
 def lambda_handler(event, context):
+    # Read query parameters from URL
+    query_params = event.get("queryStringParameters") or {}
+
+    id_param = query_params.get("id")
+    status_param = query_params.get("status")
+
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Status: OK"})
+        "body": json.dumps({
+            "message": "Received parameters",
+            "id": id_param,
+            "status": status_param
+        })
     }
